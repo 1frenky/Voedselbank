ca28606e9b04fab97fccc0ffdaa1d9c455d54a35
justin0174 <justinhamel@live.nl>
2017-05-29 13:57:17 +0200

    Tweede commit

--- src/Main.java
+++ src/Main.java
@@ -24,19 +24,29 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTabbedPane2 = new javax.swing.JTabbedPane();
+        jTabbedPaneGeheel = new javax.swing.JTabbedPane();
         jPanelMain = new javax.swing.JPanel();
         MainExitKnop = new java.awt.Button();
+        MainLabelWelkom = new java.awt.Label();
+        jPanelIntake = new javax.swing.JPanel();
+        IntakeExitKnop = new java.awt.Button();
+        jFileChooser1 = new javax.swing.JFileChooser();
         jPanelProd = new javax.swing.JPanel();
+        ProdExitKnop = new java.awt.Button();
         jPanelUitgift = new javax.swing.JPanel();
+        UitgifteExitKnop = new java.awt.Button();
         jPanelClient = new javax.swing.JPanel();
+        ClientExitKnop = new java.awt.Button();
         jPanelManage = new javax.swing.JPanel();
+        ManageExitKnop = new java.awt.Button();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Database app Voedselbank");
         setLocationByPlatform(true);
         setMaximumSize(new java.awt.Dimension(700, 700));
         setMinimumSize(new java.awt.Dimension(700, 700));
+        setPreferredSize(new java.awt.Dimension(700, 700));
+        setResizable(false);
 
         MainExitKnop.setLabel("Exit");
         MainExitKnop.addActionListener(new java.awt.event.ActionListener() {
@@ -45,86 +55,176 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
+        MainLabelWelkom.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        MainLabelWelkom.setText("Welkom bij de Voedselbank applicatie.");
+
         javax.swing.GroupLayout jPanelMainLayout = new javax.swing.GroupLayout(jPanelMain);
         jPanelMain.setLayout(jPanelMainLayout);
         jPanelMainLayout.setHorizontalGroup(
             jPanelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelMainLayout.createSequentialGroup()
-                .addContainerGap(605, Short.MAX_VALUE)
-                .addComponent(MainExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(jPanelMainLayout.createSequentialGroup()
+                .addGroup(jPanelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelMainLayout.createSequentialGroup()
+                        .addContainerGap(627, Short.MAX_VALUE)
+                        .addComponent(MainExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanelMainLayout.createSequentialGroup()
+                        .addGap(233, 233, 233)
+                        .addComponent(MainLabelWelkom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanelMainLayout.setVerticalGroup(
             jPanelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelMainLayout.createSequentialGroup()
-                .addContainerGap(625, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(MainLabelWelkom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 595, Short.MAX_VALUE)
                 .addComponent(MainExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
-        jTabbedPane2.addTab("Main menu", jPanelMain);
+        jTabbedPaneGeheel.addTab("Main menu", jPanelMain);
+
+        IntakeExitKnop.setLabel("Exit");
+        IntakeExitKnop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                IntakeExitKnopActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanelIntakeLayout = new javax.swing.GroupLayout(jPanelIntake);
+        jPanelIntake.setLayout(jPanelIntakeLayout);
+        jPanelIntakeLayout.setHorizontalGroup(
+            jPanelIntakeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelIntakeLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(IntakeExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(jPanelIntakeLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jFileChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, 685, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+        jPanelIntakeLayout.setVerticalGroup(
+            jPanelIntakeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelIntakeLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jFileChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 172, Short.MAX_VALUE)
+                .addComponent(IntakeExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        jTabbedPaneGeheel.addTab("Toevoegen Intakelijst", jPanelIntake);
+
+        ProdExitKnop.setLabel("Exit");
+        ProdExitKnop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ProdExitKnopActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanelProdLayout = new javax.swing.GroupLayout(jPanelProd);
         jPanelProd.setLayout(jPanelProdLayout);
         jPanelProdLayout.setHorizontalGroup(
             jPanelProdLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 695, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelProdLayout.createSequentialGroup()
+                .addContainerGap(627, Short.MAX_VALUE)
+                .addComponent(ProdExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanelProdLayout.setVerticalGroup(
             jPanelProdLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 670, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelProdLayout.createSequentialGroup()
+                .addContainerGap(625, Short.MAX_VALUE)
+                .addComponent(ProdExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        jTabbedPane2.addTab("Productielijst", jPanelProd);
+        jTabbedPaneGeheel.addTab("Productielijst", jPanelProd);
+
+        UitgifteExitKnop.setLabel("Exit");
 
         javax.swing.GroupLayout jPanelUitgiftLayout = new javax.swing.GroupLayout(jPanelUitgift);
         jPanelUitgift.setLayout(jPanelUitgiftLayout);
         jPanelUitgiftLayout.setHorizontalGroup(
             jPanelUitgiftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 695, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelUitgiftLayout.createSequentialGroup()
+                .addContainerGap(627, Short.MAX_VALUE)
+                .addComponent(UitgifteExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanelUitgiftLayout.setVerticalGroup(
             jPanelUitgiftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 670, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelUitgiftLayout.createSequentialGroup()
+                .addContainerGap(625, Short.MAX_VALUE)
+                .addComponent(UitgifteExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        jTabbedPane2.addTab("Uitgiftpunten", jPanelUitgift);
+        jTabbedPaneGeheel.addTab("Uitgiftpunten", jPanelUitgift);
+
+        ClientExitKnop.setLabel("Exit");
+        ClientExitKnop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ClientExitKnopActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanelClientLayout = new javax.swing.GroupLayout(jPanelClient);
         jPanelClient.setLayout(jPanelClientLayout);
         jPanelClientLayout.setHorizontalGroup(
             jPanelClientLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 695, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelClientLayout.createSequentialGroup()
+                .addContainerGap(627, Short.MAX_VALUE)
+                .addComponent(ClientExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanelClientLayout.setVerticalGroup(
             jPanelClientLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 670, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelClientLayout.createSequentialGroup()
+                .addContainerGap(625, Short.MAX_VALUE)
+                .addComponent(ClientExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        jTabbedPane2.addTab("Cliënt overzicht", jPanelClient);
+        jTabbedPaneGeheel.addTab("Cliënt overzicht", jPanelClient);
+
+        ManageExitKnop.setLabel("Exit");
+        ManageExitKnop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ManageExitKnopActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanelManageLayout = new javax.swing.GroupLayout(jPanelManage);
         jPanelManage.setLayout(jPanelManageLayout);
         jPanelManageLayout.setHorizontalGroup(
             jPanelManageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 695, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelManageLayout.createSequentialGroup()
+                .addContainerGap(627, Short.MAX_VALUE)
+                .addComponent(ManageExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanelManageLayout.setVerticalGroup(
             jPanelManageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 670, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelManageLayout.createSequentialGroup()
+                .addContainerGap(625, Short.MAX_VALUE)
+                .addComponent(ManageExitKnop, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        jTabbedPane2.addTab("Management informatie", jPanelManage);
+        jTabbedPaneGeheel.addTab("Management informatie", jPanelManage);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane2)
+            .addComponent(jTabbedPaneGeheel)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane2)
+            .addComponent(jTabbedPaneGeheel)
         );
 
         pack();
@@ -133,6 +233,22 @@ public class Main extends javax.swing.JFrame {
     private void MainExitKnopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MainExitKnopActionPerformed
         System.exit(0);
     }//GEN-LAST:event_MainExitKnopActionPerformed
+
+    private void ProdExitKnopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ProdExitKnopActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_ProdExitKnopActionPerformed
+
+    private void IntakeExitKnopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_IntakeExitKnopActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_IntakeExitKnopActionPerformed
+
+    private void ClientExitKnopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ClientExitKnopActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_ClientExitKnopActionPerformed
+
+    private void ManageExitKnopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ManageExitKnopActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_ManageExitKnopActionPerformed
 
     /**
      * @param args the command line arguments
@@ -170,12 +286,20 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private java.awt.Button ClientExitKnop;
+    private java.awt.Button IntakeExitKnop;
     private java.awt.Button MainExitKnop;
+    private java.awt.Label MainLabelWelkom;
+    private java.awt.Button ManageExitKnop;
+    private java.awt.Button ProdExitKnop;
+    private java.awt.Button UitgifteExitKnop;
+    private javax.swing.JFileChooser jFileChooser1;
     private javax.swing.JPanel jPanelClient;
+    private javax.swing.JPanel jPanelIntake;
     private javax.swing.JPanel jPanelMain;
     private javax.swing.JPanel jPanelManage;
     private javax.swing.JPanel jPanelProd;
     private javax.swing.JPanel jPanelUitgift;
-    private javax.swing.JTabbedPane jTabbedPane2;
+    private javax.swing.JTabbedPane jTabbedPaneGeheel;
     // End of variables declaration//GEN-END:variables
 }
